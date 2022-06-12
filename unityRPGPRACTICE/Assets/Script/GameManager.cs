974a7cdcee42e1d37839e7b9ed8f264b7a269376
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-06-13 08:40:10 +0900

    게임 진행을 위한 데이터 Json으로 저장하는 GameManager 함수 구성

--- unityRPGPRACTICE/Assets/Script/GameManager.cs
+++ unityRPGPRACTICE/Assets/Script/GameManager.cs
@@ -3,25 +3,62 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+[System.Serializable]
+public class User
+{
+    public int AttackLevel = 1;
+    public int HPLevel = 1;
+    public int AGILevel = 1;
+    public bool Item1 = false;
+    public bool Item2 = false;
+    public bool Item3 = false;
+    public bool Item4 = false;
+}
+
 public class GameManager : MonoSingleton<GameManager>
 {
-    [SerializeField]
-    public static User userData;
+    public User userData;
 
-    string ToJsonData = JsonUtility.ToJson(userData);
-    string path = Application.dataPath + "/SaveData";
-    
+
+
     public void SaveToJson()
     {
-        File.WriteAllText(path, ToJsonData);
+        string ToJsonData = JsonUtility.ToJson(userData);
+        string path = Application.dataPath + "/SaveData";
+        string filepath = Application.dataPath;
+
+        Debug.Log(ToJsonData);
+
+        if (!File.Exists(path))
+        {
+            Directory.CreateDirectory(filepath+"/SaveData");
+        }
+        File.WriteAllText(path+"/SaveData.Json", ToJsonData);
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
+        userData.AttackLevel = 2;
+        SaveToJson();
+    }
+
+    private void Start()
+    {
+        SaveToJson();
+        StartCoroutine(Wait());
     }
 
     public void LoadToJson()
     {
+        string path = Application.dataPath + "/SaveData";
+        string ToJsonData = JsonUtility.ToJson(userData);
+
         if (File.Exists(path))
         {
             string FromJsonData = File.ReadAllText(path);
             userData = JsonUtility.FromJson<User>(FromJsonData);
+            Debug.Log(userData);
         }
     }
 
