a35a7b86c9ad477bae2888cd7286288b504b44d6
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-06-13 00:50:54 +0900

    상점을 위한 Json 저장 구현중 및 상점 UI점검중

--- unityRPGPRACTICE/Assets/Script/GameManager.cs
+++ unityRPGPRACTICE/Assets/Script/GameManager.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    [SerializeField]
+    public static User userData;
+
+    string ToJsonData = JsonUtility.ToJson(userData);
+    string path = Application.dataPath + "/SaveData";
+    
+    public void SaveToJson()
+    {
+        File.WriteAllText(path, ToJsonData);
+    }
+
+    public void LoadToJson()
+    {
+        if (File.Exists(path))
+        {
+            string FromJsonData = File.ReadAllText(path);
+            userData = JsonUtility.FromJson<User>(FromJsonData);
+        }
+    }
+
     public enum GameState {Default ,StartGame , Gaming , EndGame}
     GameState nowState = GameState.Default;
 
