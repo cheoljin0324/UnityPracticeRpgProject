9788b2a8d4c002eca07d111f3da3a551b65d1f85
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-06-12 22:13:10 +0900

    상점 한걸 내가 푸쉬를 안했나..?

---
+++ My project (1)/Assets/FileTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileTest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameSet;
+
+    private void Start()
+    {
+        
+    }
+
+    IEnumerator Practice()
+    {
+        yield return null;
+    }
+}
